3f967feefc5f658d7ffff67da43bc98e9cef96fd
Sergey Mukhin <sergeu.m.mail@gmail.com>
2026-06-11 16:16:11 +0300

    Добавление xUnit проекта и тестирование

---
+++ SocialNetwork.Tests/FriendAddingDataTests.cs
@@ -0,0 +1,19 @@
+﻿using SocialNetwork.BLL.Models;
+
+namespace SocialNetwork.Tests
+{
+    public class FriendAddingDataTests
+    {
+        [Fact]
+        public void DataStored()
+        {
+            var friendAddingData = new FriendAddingData();
+
+            friendAddingData.UserId = 1;
+            friendAddingData.FriendEmail = "friend@mail.ru";
+
+            Assert.Equal(1, friendAddingData.UserId);
+            Assert.Equal("friend@mail.ru", friendAddingData.FriendEmail);
+        }
+    }
+}
